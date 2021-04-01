39808c3ac0104252dd66415b85d267ac66ab0086
Dan Bushong <bushongdan@gmail.com>
2021-04-01 12:47:15 -0500

    initial file add for react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { ChatEngine } from "react-chat-engine"
+import './App.css';
+
+function App() {
+  return (
+    <ChatEngine 
+      height="100vh"
+      projectID="ea523e22-cdb3-46f4-8e20-22e844950899"
+      userName="danbushong"
+      userSecret="merrychristmas"
+    
+    
+    />
+  );
+}
+
+export default App;
